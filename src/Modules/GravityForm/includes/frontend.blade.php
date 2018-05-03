eda7aa9482ffcf3341e0a94c86a059a4b434176c
Tang Rufus <tangrufus@gmail.com>
2018-05-04 01:58:24 +0800

    GravityForm: Add null checks and type cast

--- src/Modules/GravityForm/includes/frontend.blade.php
+++ src/Modules/GravityForm/includes/frontend.blade.php
@@ -1,3 +1,11 @@
+@php
+    $id = absint($settings->form_id);
+    $title = (bool) ($settings->show_title ?? true);
+    $description = (bool) ($settings->show_descr ?? true);
+    $ajax = (bool) ($settings->enable_ajax ?? false);
+    $tabIndex = (int) ($settings->tab_index ?? 1);
+@endphp
+
 @if(!empty($settings->form_id))
-    {!! gravity_form(absint($settings->form_id), $settings->show_title, $settings->show_descr, false, null, $settings->enable_ajax, $settings->tab_index, false) !!}
+    {!! gravity_form($id, $title, $description, false, null, $ajax, $tabIndex, false) !!}
 @endif

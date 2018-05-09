ItinerisLtd/sage-flbuilder

commit 37460a8fa89d765bd4d323955e6afd27a114f2a2

Extract `God::loadPageBladeTemplate` into smaller classes
Breaking: WooCommerce theme layout is now disabled by default.

src/SageFLBuilder.php
@@ -21,6 +21,8 @@ use Itineris\SageFLBuilder\Modules\Video\Video;
 use Itineris\SageFLBuilder\Settings\EventsArchive;
 use Itineris\SageFLBuilder\Settings\PostGrid;
 use Itineris\SageFLBuilder\Settings\ProductsArchive;
+use Itineris\SageFLBuilder\Settings\ThemeLayouts\ArchiveThemeLayout;
+use Itineris\SageFLBuilder\Settings\ThemeLayouts\HomeThemeLayout;
 use function App\sage;
 
 /**
@@ -31,14 +33,16 @@ class SageFLBuilder
     protected const DEFAULT_INITIALIZABLES = [
         Accordion::class,
         Alert::class,
+        ArchiveThemeLayout::class,
         Breadcrumbs::class,
         Button::class,
         ContentImage::class,
         EventsArchive::class,
         FilterBar::class,
         Gallery::class,
-        GravityForm::class,
         God::class,
+        GravityForm::class,
+        HomeThemeLayout::class,
         PageHeading::class,
         PageSlider::class,
         PostGrid::class,

src/Settings/ThemeLayouts/AbstractThemeLayout.php
@@ -1,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Itineris\SageFLBuilder\Settings\ThemeLayouts;
+
+use Itineris\SageFLBuilder\AbstractHelper;
+use Itineris\SageFLBuilder\InitializableInterface;
+use function App\sage;
+
+abstract class AbstractThemeLayout implements InitializableInterface
+{
+    protected const POST_TYPE = 'fl-theme-layout';
+    protected const PRIORITY = 1000000;
+    protected const TEMPLATE = 'fl-builder-archive';
+
+    public static function init(): void
+    {
+        add_filter('fl_theme_builder_template_include', static::class . '::locateTemplatePath', static::PRIORITY);
+    }
+
+    public static function locateTemplatePath(string $template): string
+    {
+        if (static::POST_TYPE !== get_post_type()) {
+            return $template;
+        }
+
+        if (! self::shouldIncludeLayout()) {
+            return $template;
+        }
+
+        /** @var AbstractHelper $helper */
+        $helper = sage(AbstractHelper::class);
+
+        return $helper->templatePath(
+            $helper->locateTemplate(self::TEMPLATE)
+        );
+    }
+
+    abstract protected static function shouldIncludeLayout(): bool;
+}

src/God.php
@@ -21,8 +21,6 @@ class God implements InitializableInterface
         add_action('fl_builder_posts_module_after_pagination', [$god, 'noPostsFilterBar'], 10, 2);
 
         add_filter('fl_builder_loop_settings', [$god, 'forceEventPostType']);
-        // TODO: Is `PHP_INT_MAX` necessary?
-        add_filter('fl_theme_builder_template_include', [$god, 'loadPageBladeTemplate'], PHP_INT_MAX);
         add_filter('fl_builder_render_module_content', [$god, 'wrapRichText'], 10, 2);
         add_filter('fl_builder_module_frontend_custom_fab_filter_bar', [$god, 'filterBarFrontend']);
     }
@@ -98,29 +96,6 @@ class God implements InitializableInterface
         }
 
         return $settings;
-    }
-
-    /**
-     * TODO: Review if/else conditions.
-     */
-    public function loadPageBladeTemplate($template)
-    {
-        /** @var AbstractHelper $helper */
-        $helper = sage(AbstractHelper::class);
-
-        $postType = get_post_type();
-        if ('fl-theme-layout' === $postType || $this->isWoocommerce()) {
-            $template = $helper->templatePath($helper->locateTemplate('woocommerce/fl-builder-woocommerce'));
-        } elseif ('fl-theme-layout' === $postType || is_home() || is_archive() || is_404() || is_search()) {
-            $template = $helper->templatePath($helper->locateTemplate('fl-builder-archive'));
-        }
-
-        return $template;
-    }
-
-    private function isWoocommerce(): bool
-    {
-        return function_exists('is_woocommerce') && is_woocommerce();
     }
 
     /**

src/Settings/ThemeLayouts/ArchiveThemeLayout.php
@@ -1,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Itineris\SageFLBuilder\Settings\ThemeLayouts;
+
+class ArchiveThemeLayout
+{
+    protected static function shouldIncludeLayout(): bool
+    {
+        return is_archive();
+    }
+}

src/Settings/ThemeLayouts/FourOFourThemeLayout.php
@@ -1,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Itineris\SageFLBuilder\Settings\ThemeLayouts;
+
+class FourOFourThemeLayout extends AbstractThemeLayout
+{
+    protected static function shouldIncludeLayout(): bool
+    {
+        return is_404();
+    }
+}

src/Settings/ThemeLayouts/HomeThemeLayout.php
@@ -1,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Itineris\SageFLBuilder\Settings\ThemeLayouts;
+
+class HomeThemeLayout extends AbstractThemeLayout
+{
+    protected static function shouldIncludeLayout(): bool
+    {
+        return is_home();
+    }
+}

src/Settings/ThemeLayouts/SearchThemeLayout.php
@@ -1,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Itineris\SageFLBuilder\Settings\ThemeLayouts;
+
+class SearchThemeLayout extends AbstractThemeLayout
+{
+    protected static function shouldIncludeLayout(): bool
+    {
+        return is_search();
+    }
+}

src/Settings/ThemeLayouts/WooCommerceThemeLayout.php
@@ -1,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Itineris\SageFLBuilder\Settings\ThemeLayouts;
+
+class WooCommerceThemeLayout extends AbstractThemeLayout
+{
+    protected const PRIORITY = parent::PRIORITY + 10;
+    protected const TEMPLATE = 'woocommerce/fl-builder-woocommerce';
+
+    protected static function shouldIncludeLayout(): bool
+    {
+        return is_woocommerce();
+    }
+}

commit 8d9908a9b0c62f8f774eff1bf577787877c17b0e

Fix: Theme layout settings only works for `fl-theme-layout` post type
Fixing bugs introduced in #43

src/SageFLBuilder.php
@@ -24,6 +24,7 @@ use Itineris\SageFLBuilder\Settings\RichText;
 use Itineris\SageFLBuilder\Settings\ThemeLayouts\ArchiveThemeLayout;
 use Itineris\SageFLBuilder\Settings\ThemeLayouts\HomeThemeLayout;
 use function App\sage;
+use Itineris\SageFLBuilder\Settings\ThemeLayouts\ThemeLayout;
 
 /**
  * Beaver Builder extensions
@@ -50,6 +51,7 @@ class SageFLBuilder
         SecondaryNav::class,
         Table::class,
         Testimonial::class,
+        ThemeLayout::class,
         Video::class,
     ];
 

src/Settings/ThemeLayouts/AbstractThemeLayout.php
@@ -10,7 +10,6 @@ use function App\sage;
 
 abstract class AbstractThemeLayout implements InitializableInterface
 {
-    protected const POST_TYPE = 'fl-theme-layout';
     protected const PRIORITY = 1000000;
     protected const TEMPLATE = 'fl-builder-archive';
 
@@ -21,10 +20,6 @@ abstract class AbstractThemeLayout implements InitializableInterface
 
     public static function locateTemplatePath(string $template): string
     {
-        if (static::POST_TYPE !== get_post_type()) {
-            return $template;
-        }
-
         if (! static::shouldIncludeLayout()) {
             return $template;
         }
@@ -33,7 +28,7 @@ abstract class AbstractThemeLayout implements InitializableInterface
         $helper = sage(AbstractHelper::class);
 
         return $helper->templatePath(
-            $helper->locateTemplate(self::TEMPLATE)
+            $helper->locateTemplate(static::TEMPLATE)
         );
     }
 

src/Settings/ThemeLayouts/ThemeLayout.php
@@ -1,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Itineris\SageFLBuilder\Settings\ThemeLayouts;
+
+class ThemeLayout extends AbstractThemeLayout
+{
+    protected const PRIORITY = parent::PRIORITY + 20;
+
+    protected static function shouldIncludeLayout(): bool
+    {
+        return 'fl-theme-layout' === get_post_type();
+    }
+}
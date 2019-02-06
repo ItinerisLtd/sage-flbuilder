819fb1f4b9fea4ac023c45f33d6b2fafeb0ec2b9
BufferOverNo <volatiledesign@outlook.com>
2019-02-06 14:52:17 +0000

    Fix GF Module

--- src/Modules/GravityForm/includes/frontend.blade.php
+++ src/Modules/GravityForm/includes/frontend.blade.php
@@ -1,9 +1,9 @@
 @php
     $id = absint($settings->form_id);
-    $title = (bool) ($settings->show_title ?? true);
-    $description = (bool) ($settings->show_descr ?? true);
-    $ajax = (bool) ($settings->enable_ajax ?? false);
-    $tabIndex = (int) ($settings->tab_index ?? 1);
+    $title = (bool) ($settings->show_title ? true : false);
+    $description = (bool) ($settings->show_descr ? true : false);
+    $ajax = (bool) ($settings->enable_ajax ? true : false);
+    $tabIndex = (int) ($settings->tab_index ? 1 : 0);
 @endphp
 
 @if(!empty($settings->form_id))

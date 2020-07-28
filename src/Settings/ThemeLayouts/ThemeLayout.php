4657bd72e5598efaa80adc7c35e8d79e1c2a236d
codepuncher <lee.hanbury@gmail.com>
2020-07-28 15:03:46 +0100

    Pass data to templates and type properties

--- src/Settings/ThemeLayouts/ThemeLayout.php
+++ src/Settings/ThemeLayouts/ThemeLayout.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Itineris\SageFLBuilder\Settings\ThemeLayouts;
 
 use Closure;
+use FLBuilderModel;
+use FLThemeBuilderLayoutData;
 
 use function Roots\view;
 
@@ -13,17 +15,17 @@ final class ThemeLayout
     /**
      * @var Closure
      */
-    private $shouldInclude;
+    private Closure $shouldInclude;
 
     /**
      * @var string
      */
-    private $template;
+    private string $template;
 
     public function __construct(Closure $shouldInclude, string $template)
     {
         $this->shouldInclude = $shouldInclude;
-        $this->template = $template;
+        $this->template      = $template;
     }
 
     public function locateTemplatePath(string $template): string
@@ -32,12 +34,26 @@ final class ThemeLayout
             return $template;
         }
 
-        return view()->exists($this->template)
-            ? view($this->template)->makeLoader()
-            : (
-                view()->exists("ItinerisSageFLBuilderViews::{$this->template}")
-                ? view("ItinerisSageFLBuilderViews::{$this->template}")->makeLoader()
-                : $template
-            );
+        $ids = FLThemeBuilderLayoutData::get_current_page_content_ids();
+
+        if ('fl-theme-layout' === get_post_type() && count($ids) > 1) {
+            $post_id = FLBuilderModel::get_post_id();
+        } else {
+            $post_id = $ids[0] ?? get_the_ID();
+        }
+
+        $data = [
+            'post_id' => $post_id,
+        ];
+
+        if (view()->exists($this->template)) {
+            return view($this->template, $data)->makeLoader();
+        }
+
+        if (view()->exists("ItinerisSageFLBuilderViews::{$this->template}")) {
+            return view("ItinerisSageFLBuilderViews::{$this->template}", $data)->makeLoader();
+        }
+
+        return $template;
     }
 }

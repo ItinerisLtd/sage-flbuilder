0e4def94ff5f58ef1033330cdc1b82d858452535
codepuncher <lee.hanbury@gmail.com>
2020-07-28 16:00:26 +0100

    Rename view namespaces

--- resources/views/fl-builder-single.blade.php
+++ resources/views/fl-builder-single.blade.php
@@ -1,4 +1,4 @@
-@extends('ItinerisSageFLBuilderViews::layouts.archive')
+@extends('ItinerisSageFLBuilder::layouts.archive')
 
 @section('content')
   @php FLBuilder::render_content_by_id($post_id, 'div', apply_filters('fl_theme_builder_content_attrs', [])); @endphp

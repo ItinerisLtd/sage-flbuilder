23d26b28158e99fc067ac4cb032923c42e7975a7
codepuncher <lee.hanbury@gmail.com>
2025-01-20 15:16:36 +0000

    fix(Button): use valid class sanitisation function

--- src/Modules/Button/includes/frontend.php
+++ src/Modules/Button/includes/frontend.php
@@ -14,6 +14,6 @@ $helper = sage(AbstractHelper::class);
 ?>
 <?php if (! empty($settings->link) && ! empty($settings->text)) : ?>
     <a href="<?php echo esc_url($settings->link); ?>"
-       class="btn <?php echo $helper->sanitizeHtmlClasses($settings->style); ?>"
+       class="btn <?php echo sanitize_html_class($settings->style); ?>"
        target="<?php echo esc_attr($settings->target); ?>"><?php echo esc_html($settings->text); ?></a>
 <?php endif; ?>
